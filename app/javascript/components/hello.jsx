c6ec435fca403190f88391d4a210ea17b64a4e9a
acrojas2 <alejandra@platan.us>
2021-08-11 12:47:10 -0400

    feat(): add dummy component

---
+++ app/javascript/components/hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DailyIframe from '@daily-co/daily-js';
+
+export default function Hello() {
+  return (
+    <div>
+      Holaaaaaa soy un componente de react
+    </div>
+  )
+}
